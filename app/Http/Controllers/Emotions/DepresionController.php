c6d08e4bf0115859f139231c9cb22dcef6bb9cb5
LuisOrtiz12 <126093125+LuisOrtiz12@users.noreply.github.com>
2023-03-03 23:03:14 -0500

    Update DepresionController.php

--- app/Http/Controllers/Emotions/DepresionController.php
+++ app/Http/Controllers/Emotions/DepresionController.php
@@ -57,7 +57,7 @@ class DepresionController extends Controller
         ]);
 
         $ira= $request ->validate([
-            'video' => ['file'],
+            'video' => ['required','file','mimes:mp4','max:200000'],
         ]);
         $file = $ira['video'];
         $uploadedFileUrl = Cloudinary::uploadVideo($file->getRealPath(),['folder'=>'emotions']);
